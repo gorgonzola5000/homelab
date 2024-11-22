a4bc1018bec8d74aa51bfddc6c707c31b24700e5
starfruit6000 <starfruit6000@tuta.com>
2024-11-22 23:15:53 +0100

    provider setup enhancement

--- terraform/dev/terraform.tf
+++ terraform/dev/terraform.tf
@@ -3,12 +3,37 @@ terraform {
   }
   required_providers {
     proxmox = {
-      version = "0.66.1"
+      version = "0.67.0"
       source  = "bpg/proxmox"
     }
+
     local = {
       source  = "hashicorp/local"
       version = "2.5.2"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
   }
 }
+
+provider "cloudflare" {
+}
+
+provider "proxmox" {
+  endpoint = "https://proxmox.${var.subdomain}.${var.domain}"
+  # username and password set with env variables
+
+  ssh {
+    agent       = false
+    username    = "terraform"
+    private_key = base64decode(var.terraform_private_key)
+  }
+}
+
+variable "terraform_private_key" {
+  type = string
+}
+
